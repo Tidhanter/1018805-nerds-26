ddb83be170845b0ae2039866891cc821cd452916
Tidhanter <9reshnik@i.ua>
2019-11-03 21:51:53 +0200

    Личный проект: завершаем вёрстку

--- js/modal.js
+++ js/modal.js
@@ -10,45 +10,45 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-	storage = localStorage.getItem("login");
+  storage = localStorage.getItem("login");
 } catch (err) {
-	isStorageSupport = false;
+  isStorageSupport = false;
 }
 
 link.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.add("modal-show");
+  evt.preventDefault();
+  popup.classList.add("modal-show");
 
-	if (storage) {
-		login.value = storage;
+  if (storage) {
+    login.value = storage;
 
-}
+  }
 });
 
 close.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.remove("modal-show");
-	popup.classList.remove("modal-error");
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
-	if (!login.value || !email.value) {
-	evt.preventDefault();
-	popup.classList.add("modal-error");
-	console.log("Нужно ввести имя и email");
-} else {
-	if (isStorageSupport) {
-	localStorage.setItem("login", login.value);
-}
-}
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести имя и email");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
 });
 
-window.addEventListener("keydown" , function(evt) {
-	if (evt.keyCode === 27) {
-		evt.preventDefault();
-		if (popup.classList.contains("modal-show")) {
-		   popup.classList.remove("modal-show");
-		   popup.classList.remove("modal-error");
-		}
-	}
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
